6f6f15e3ea46d2520eee617ac550c277125bd1c4
Fairvinay <fairvinay@gmail.com>
2025-02-10 20:33:45 +0530

    cors update and viewer js align

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     CORS_RESOURCES = {
-        '/remove_password': {'origins': 'https://storenotify.in,http://localhost:port,http://127.0.0.1:5000/'}
+        '/remove_password': {'origins': '*'}
     }
